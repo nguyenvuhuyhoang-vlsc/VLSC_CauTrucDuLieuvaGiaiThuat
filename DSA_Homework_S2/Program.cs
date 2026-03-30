b74545db33ab525515ce9cd78af6b86cceba51c8
nguyenvuhuyhoang-vlsc <2500113459@vanlangsaigon.edu.vn>
2026-03-30 11:25:33 +0700

    Add files via upload

---
+++ DSA_Homework_S2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static int TinhTong(int n)
+    {
+        if (n == 1) return 1;
+        return n + TinhTong(n - 1);
+    }
+
+    static long GiaiThuaVongLap(int n)
+    {
+        long KetQua = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            KetQua += i;
+        }
+        return KetQua;
+    }
+
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.Write("Nhập n để tính tổng: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine($"---Tổng S({n}) = {TinhTong(n)}");
+        Console.WriteLine($"Giai thua cua {n} la {GiaiThuaVongLap(n)}");
+    }
+}
